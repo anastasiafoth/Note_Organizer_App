9d0a26104d497e2b02891c6db75e7b77ceb1cc48
anastasiafoth <anastasiafoth9@gmail.com>
2026-01-19 12:00:33 +0100

    added listNotes function and semicolons

--- note_app.js
+++ note_app.js
@@ -18,30 +18,44 @@ let option = readline.question(`
 function readFile() {
     let notesData = fs.readFileSync(FILEPATH, "utf8");
     let notesParsed = JSON.parse(notesData);
-    return notesParsed
+    return notesParsed;
 }
 
 function writeFile(notes) {
     const json = JSON.stringify(notes, null, 2);
     fs.writeFileSync(FILEPATH, json);
-    console.log("Note added successfully!")
+    console.log("Note added successfully!");
 }
 
     
 function addNote () {
     let userTitle = readline.question("Enter note title: ");
-    let userBody = readline.question("Enter note body: ")
+    let userBody = readline.question("Enter note body: ");
     let userTime = new Date().toISOString();
 
     let newNote = {
         title : userTitle,
         body : userBody,
         time_added : userTime
-    }
+    };
 
-    let notes = readFile()
-    notes.push(newNote)
+    let notes = readFile();
+    notes.push(newNote);
 
-    writeFile(notes)  
-}
+    writeFile(notes);  
+};
 
+
+function listNotes() {
+    let notes = readFile();
+
+    for (let [index, note] of Object.entries(notes)) {
+        index = Number(index) + 1
+        console.log(
+            `${index}. Title: ${note.title} \n`+
+            `   Body: ${note.body} \n`+
+            `   Added on: ${note.time_added} \n`)
+    };
+};
+
+listNotes()
